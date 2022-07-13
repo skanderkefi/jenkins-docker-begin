6ce4edb85b0286d2c707d9ad5aefe9d405bbe65d
Kefiskander0 <skander.kefi@esprit.tn>
2022-07-13 22:59:27 +0100

    first commit

---
+++ src/main/java/com/example/demo/JenkinsDockerBeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerBeginApplication.class, args);
+	}
+
+}
